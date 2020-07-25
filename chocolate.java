63958c7d73ad19ebc49f2a974732a27312857124
sharathkumar555 <mano362480@gmail.com>
2020-07-25 13:40:06 +0530

    first commit

---
+++ chocolate.java
@@ -0,0 +1,20 @@
+package choco;
+import java.util.*;
+import sweet.sweets;
+public class chocolate extends sweets{
+	Scanner sc=new Scanner(System.in);
+	public void choco1()
+	{
+		System.out.println("enter no of chocolates of type 1 : ");
+		int c1=sc.nextInt();
+		System.out.println("choco 1 : "+c1);
+		ct=ct+c1;/
+	}
+	public void choco2()
+	{
+		System.out.println("enter no of chocolates of type 2 : ");
+		int c2=sc.nextInt();
+		System.out.println("choco 1 : "+c2);
+		ct=ct+c2;
+	}
+}
